8a8f2e5331e15b309e68857299ee7e586865397b
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2024-12-03 15:29:54 -0600

    Okay, so I hope this works. I was using 2 computers. files dissappeared, then appeared, then had to be remade and appeared again, it was crazy. Had to delete lots of files.

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/* global $ */
+
+$(function () {
+    const amenities = {};
+  
+    $('div.amenities input[type="checkbox"]').change(function () {
+      const element = $(this);
+      const dataId = element.attr('data-id');
+      const dataName = element.attr('data-name');
+  
+      if (element.is(':checked')) {
+        amenities[dataId] = dataName;
+      } else {
+        delete amenities[dataId];
+      }
+  
+      const amenityValues = Object.values(amenities);
+      $('div.amenities h4').text(amenityValues);
+    });
+  });
---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,101 @@
+#!/usr/bin/node
+/* global $ */
+
+$(function () {
+    const amenities = {};
+  
+    $('div.amenities input[type="checkbox"]').change(function () {
+      const element = $(this);
+      const dataId = element.attr('data-id');
+      const dataName = element.attr('data-name');
+  
+      if (element.is(':checked')) {
+        amenities[dataId] = dataName;
+      } else {
+        delete amenities[dataId];
+      }
+  
+      const amenityValues = Object.values(amenities);
+      $('div.amenities h4').text(amenityValues);
+    });
+  
+    $.get('http://localhost:5001/api/v1/status/', function (data, stat) {
+      if (data.status === 'OK') {
+        $('div#api_status').addClass('available');
+      } else {
+        $('div#api_status').removeClass('available');
+      }
+    });
+  
+    $.ajax({
+      url: 'http://localhost:5001/api/v1/places_search',
+      type: 'POST',
+      contentType: 'application/json',
+      data: '{}', // empty dict
+      success: function (places) {
+        $('section.places').empty(); // clear the section
+  
+        for (const place of places) { // loop through the places
+          const articlePlaces = `
+          <article>
+            <div class="title_box">
+              <h2>${place.name}</h2>
+              <div class="price_by_night">$${place.price_by_night}</div>
+            </div>
+            <div class="information">
+              <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+              <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+              <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+            </div>
+            <div class="description">
+              ${place.description}
+            </div>
+          </article>`;
+          $('section.places').append(articlePlaces); // append the article to the section
+        }
+      },
+      error: function (error) {
+        console.error('Error fetching places:', error);
+      }
+    });
+  
+    $('button').click(function () { // button click
+      const checkedAmenities = [];
+      $('div.amenities input[type="checkbox"]').each(function () {
+        if ($(this).is(':checked')) { // if checked
+          checkedAmenities.push($(this).data('id')); // push that id
+        }
+      });
+    
+      $.ajax({
+        url: 'http://localhost:5001/api/v1/places_search/',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({ amenities: checkedAmenities }),
+        success: function (placesResponse) {
+          $('section.places').empty();
+          for (const place of placesResponse) {
+            const articlePlaces = `
+            <article>
+              <div class="title_box">
+                <h2>${place.name}</h2>
+                <div class="price_by_night">$${place.price_by_night}</div>
+              </div>
+              <div class="information">
+                <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+                <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+                <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+              </div>
+              <div class="description">
+                ${place.description}
+              </div>
+            </article>`;
+            $('section.places').append(articlePlaces);
+          }
+        },
+        error: function (error) {
+          console.error('Error fetching places:', error);
+        }
+      });
+    });
+  });
